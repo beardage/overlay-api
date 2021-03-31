2d985f97566b58dcb65bae01bc9abe05c632fb46
beardage <xypress@gmail.com>
2021-03-30 22:44:58 -0700

    adding tmi and simple command

--- app.ts
+++ app.ts
@@ -2,14 +2,39 @@ import express from 'express';
 import * as http from 'http';
 import {CommonRoutesConfig} from './common/common.routes.config';
 import {UsersRoutes} from './users/users.routes.config';
+
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = 3000;
 const routes: any = [];
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+	options: { debug: true, messagesLogLevel: "info" },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: 'beardage',
+		password: `${process.env.TWITCH_BOT_OAUTH}`,
+	},
+	channels: [ 'beardage' ]
+});
+client.connect().catch(console.error);
+client.on('message', (channel:any, tags:any, message:any, self:any) => {
+	if(self) return;
+	if(message.toLowerCase() === '!hello') {
+		client.say(channel, `@${tags.username}, heya!`);
+	}
+});
+
 routes.push(new UsersRoutes(app));
+
 app.get('/', (req: express.Request, res: express.Response) => {
     res.status(200).send(`Server running at port ${port}`)
 });
+
 server.listen(port, () => {
     console.log(`Server running at port ${port}`);
     routes.forEach((route: CommonRoutesConfig) => {
