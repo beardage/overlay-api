acb260b95ca0d7357832978ee6f7517f1914f7dd
beardage <xypress@gmail.com>
2021-04-04 00:33:51 -0700

    adding dao for chat, starting with todo methods

---
+++ daos/chat.dao.ts
@@ -0,0 +1,60 @@
+export class chatDao {
+	private static instance: chatDao;
+
+	constructor () {
+		console.log('created new chatDao');
+	}
+
+	static getInstance(): chatDao {
+		if(!chatDao.instance) {
+			chatDao.instance = new chatDao();
+		}
+		return chatDao.instance;
+	}
+	
+	addTodo(todo: any) {
+		return this.todos.push(todo);
+	}
+
+	getTodos() {
+		return this.todos;
+	}
+
+	getTodoById(todoId: string) {
+		return this.todos.find((todo: {id: string; }) => todo.id === todoId);
+	}
+
+	putTodoById(todo: any) {
+		const objIndex = this.todos.findIndex((obj: { id: any; }) => obj.id === todo.id);
+		const updatedTodos = [
+			...this.todos.slice(0, objIndex),
+			todo,
+			...this.todos.slice(objIndex + 1),
+		];
+		this.todos = updatedTodos;
+		return `${todo.id} updated via put`;
+	}
+
+	patchTodoById(todo: any) {
+		const objIndex = this.todos.findIndex((obj: [id: any; }) => obj.id === todo.id);	
+		let currentTodo = this.todos[objIndex];
+		for (let i in todo) {
+			if (i !== 'id') {
+				currentTodo[i] = todo[i];
+			}
+		}
+		this.todos = [
+			...this.todos.slice(0, objIndex),
+			currentTodo,
+			...this.todos.slice(objIndex + 1),
+		];
+		return `${user.id} patched`;
+	}
+
+	removeTodoById(todoId: string) {
+		const objIndex = this.todos.findIndex((obj: { id: any; }) => obj.id === todoId);
+		this.todos = this.todos.splice(objIndex, 1);
+		return `${todoId} removed`;
+	}
+
+}
