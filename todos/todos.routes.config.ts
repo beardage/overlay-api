80fcfb5eeabd2068b6aa329dc640d124119ff240
beardage <xypress@gmail.com>
2021-04-05 23:49:47 -0700

    adding routes for todos

---
+++ todos/todos.routes.config.ts
@@ -0,0 +1,35 @@
+import {CommonRoutesConfig, configureRoutes} from '../common/common.routes.config'
+import express from 'express';
+
+export class TodosRoutes extends CommonRoutesConfig implements configureRoutes {
+	constructor(app: express.Application) {
+		super(app, 'TodosRoute');
+		this.configureRoutes();
+	}
+
+	configureRoutes() {
+		this.app.get(`/todos`, (req: express.Request, res: express.Response) => {
+			res.status(200).send(`List of todos`);
+		});
+
+		this.app.post(`/todos`, (req: express.Request, res: express.Response) => {
+			res.status(200).send(`Post to todos`);
+		});
+
+		this.app.put(`/todos/:todoId`, (req: express.Request, res: express.Response) => {
+			res.status(200).send(`Put to ${req.params.todoId}`);
+		});
+
+		this.app.patch(`/todos/:todoId`, (req: express.Request, res: express.Response) => {
+			res.status(200).send(`Patch to ${req.params.todoId}`);
+		});
+
+		this.app.delete(`/todos/:todoId`, (req: express.Request, res: express.Response) => {
+			res.status(200).send(`Delete to ${req.params.todoId}`);
+		});
+
+		this.app.get(`/todos/:todoId`, (req: express.Request, res: express.Response) => {
+			res.status(200).send(`Get to ${req.params.todoId}`);
+		});						
+	}
+}
