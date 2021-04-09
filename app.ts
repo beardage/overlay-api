30f5c914116f6388312dbeec8cc4334636396516
beardage <xypress@gmail.com>
2021-04-09 00:07:06 -0700

    adding json body parsing to express

--- app.ts
+++ app.ts
@@ -1,35 +1,37 @@
-import express from 'express';
-import * as http from 'http';
-import {CommonRoutesConfig} from './common/common.routes.config';
-import {TodosRoutes} from './todos/todos.routes.config';
+import express from "express";
+import * as http from "http";
+import { CommonRoutesConfig } from "./common/common.routes.config";
+import { TodosRoutes } from "./todos/todos.routes.config";
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = 3000;
 const routes: any = [];
 
-const tmi = require('tmi.js');
+app.use(express.json());
+
+const tmi = require("tmi.js");
 const client = new tmi.Client({
 	options: { debug: true, messagesLogLevel: "info" },
 	connection: {
 		reconnect: true,
-		secure: true
+		secure: true,
 	},
 	identity: {
-		username: 'beardage',
+		username: "beardage",
 		password: `${process.env.TWITCH_BOT_OAUTH}`,
 	},
-	channels: [ 'beardage' ]
+	channels: ["beardage"],
 });
 client.connect().catch(console.error);
-client.on('message', onMessageHandler);
+client.on("message", onMessageHandler);
 
-function onMessageHandler (target:any, context:any, msg:any, self:any) {
-	if(self) return;
+function onMessageHandler(target: any, context: any, msg: any, self: any) {
+	if (self) return;
 	// admin only commands
-	if(context.badges.broadcaster == "1") {
-		console.log('broadcaster');
-		if(msg.toLowerCase() === '!test') {
+	if (context.badges.broadcaster == "1") {
+		console.log("broadcaster");
+		if (msg.toLowerCase() === "!test") {
 			client.say(target, `@${context.username}, test functional`);
 		}
 	}
@@ -37,13 +39,13 @@ function onMessageHandler (target:any, context:any, msg:any, self:any) {
 
 routes.push(new TodosRoutes(app));
 
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.status(200).send(`Server running at port ${port}`)
+app.get("/", (req: express.Request, res: express.Response) => {
+	res.status(200).send(`Server running at port ${port}`);
 });
 
 server.listen(port, () => {
-    console.log(`Server running at port ${port}`);
-    routes.forEach((route: CommonRoutesConfig) => {
-        console.log(`Routes configured for ${route.getName()}`);
-    });
+	console.log(`Server running at port ${port}`);
+	routes.forEach((route: CommonRoutesConfig) => {
+		console.log(`Routes configured for ${route.getName()}`);
+	});
 });
